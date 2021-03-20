0f98606df05fe2634c970336ae7138861e16514f
Zeba Afia Shama <zebashama2@gmail.com>
2021-03-20 16:50:40 +0600

    Add files upto front end

---
+++ Restaurant/admin/index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>Food Order Website-Home page</title>
+<link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+<!--Menu Section Starts-->
+
+<div class="menu">
+<div class="wrapper"> 
+
+</div>
+</div>
+<!--Menu Section Ends-->
+
+
+<!--Main Content Section Starts-->
+
+<div class="main-content">
+<div class="wrapper"> 
+
+</div>
+</div>
+<!--Main Content Section Ends-->
+
+
+<!--Footer Section Starts-->
+<div class="footer">
+<div class="wrapper"> 
+<p class="text-center">2021 All rights reserved, Sharna's Bakery. Developed By- <a href="#">Zeba Afia Shama</a></p>
+</div>
+</div>
+<!--Footer Section Ends-->
+
+
+
+
+</body>
+</html>
