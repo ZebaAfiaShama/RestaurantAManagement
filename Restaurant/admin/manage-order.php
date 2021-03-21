287e2a3dcc4aba5faeaf38b4102dd240b9661cd3
ZebaAfiaShama <zebashama2@gmail.com>
2021-03-21 10:05:51 +0600

    Structure of Menu

--- Restaurant/admin/manage-order.php
+++ Restaurant/admin/manage-order.php
@@ -1,16 +1,9 @@
 <?php include('partials/menu.php');?>
-
-
-<!--Main Content Section Starts-->
-
 <div class="main-content">
-<div class="wrapper"> 
-<h1>Manage Admin</h1>
-
-
+<div class="wrapper">
+<h1>Manage Order</h1>
 </div>
 </div>
-<!--Main Content Section Ends-->
 
 
 <?php include('partials/footer.php');?>
--- Restaurant/admin/partials/footer.php
+++ Restaurant/admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!--Footer Section Starts-->
 <div class="footer">
 <div class="wrapper"> 
-<p class="text-center">2021 All rights reserved, Sharna's Bakery. Developed By- <a href="#">Zeba Afia Shama</a></p>
+<p class="text-center">2021 All rights reserved, Sharna's Bakery. Developed By- <a href="https://zebaafiashama.xyz/">Zeba Afia Shama</a></p>
 </div>
 </div>
 <!--Footer Section Ends-->
