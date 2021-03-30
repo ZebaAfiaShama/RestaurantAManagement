8f2487d40050bd90dda6d57c3a929bf0ea684cfd
ZebaAfiaShama <zebashama2@gmail.com>
2021-03-30 22:23:58 +0600

    Login Authentication and Access Authorization

--- Restaurant/admin/partials/menu.php
+++ Restaurant/admin/partials/menu.php
@@ -1,6 +1,10 @@
 <?php
  include('../config/constants.php');
+ include('login-check.php');
+
 ?>
+
+
 
 <html>
 <head>
@@ -18,6 +22,7 @@
 <li><a href="manage-category.php">Category</a></li>
 <li><a href="manage-food.php">Food</a></li>
 <li><a href="manage-order.php">Order</a></li>
+<li><a href="logout.php">Logout</a></li>
 </ul>
 </div>
 </div>
