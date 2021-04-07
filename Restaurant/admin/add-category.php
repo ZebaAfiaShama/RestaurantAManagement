622bce550fb5bff72d17b5b5a60a4c1952cee4a0
ZebaAfiaShama <zebashama2@gmail.com>
2021-04-07 12:04:17 +0600

    Update and Delete Category

--- Restaurant/admin/add-category.php
+++ Restaurant/admin/add-category.php
@@ -68,66 +68,69 @@ if(isset($_POST['submit']))
 {
     // echo "clicked";
     // 1.Get the value from Category FORM
-    $title=$_POST['title'];
-    // For radio input type we need to check whether the button is selected or not
-    if(isset($_POST['featured']))
-    {
-        //Get the value form FORM
-        $featured=$_POST['featured'];
-    }
-    else{
+      $title=$_POST['title'];
+          // For radio input type we need to check whether the button is selected or not
+          if(isset($_POST['featured']))
+             {
+                  //Get the value form FORM
+                 $featured=$_POST['featured'];
+             }
+         else{
         // Set the default value
         $featured="No";
-    }
-    if(isset($_POST['active']))
-    {
-        $active=$_POST['active'];
-    }
-    else{
-        $active="No";
-    }
+             }
+                 if(isset($_POST['active']))
+                     {
+                         $active=$_POST['active'];
+                     }
+                 else{
+                          $active="No";
+                     }
 
-    // Check whether the image is selected or not and set the value for image name accordingly
-    // print_r($_FILES['image']);
-    // die();//Break the Code More
-    if(isset($_FILES['image']['name']))
-    {
-    //Upload the image
-    // To upload image we need image name ,source path and destination path
-    $image_name=$_FILES['image']['name'];
+                       // Check whether the image is selected or not and set the value for image name accordingly
+                       // print_r($_FILES['image']);
+                       // die();//Break the Code More
+                          if(isset($_FILES['image']['name']))
+                             {
+                                  //Upload the image
+                                   // To upload image we need image name ,source path and destination path
+                                     $image_name=$_FILES['image']['name'];
 
-    // Auto rename our image
-    // 1.Get the Extension of our image (jpg,png,gif etc) e.g. "food.jpg"
-    $ext = end(explode('.', $image_name));
+                                          // Upload image only if image is selected
+                                             if($image_name!="")
+                                                 {
+                                                 // Auto rename our image
+                                                 // 1.Get the Extension of our image (jpg,png,gif etc) e.g. "food.jpg"
+                                                      $ext = end(explode('.', $image_name));
 
-    // 2.Rename the image
-    $image_name= "Food_Category_".rand(000, 999).'.'.$ext; // e.g. Food_Category_834.jpg
+                                                     // 2.Rename the image
+                                                     $image_name= "Food_Category_".rand(000, 999).'.'.$ext; // e.g. Food_Category_834.jpg
 
-    $source_path=$_FILES['image']['tmp_name'];
+                                                     $source_path=$_FILES['image']['tmp_name'];
 
-    $destination_path="../images/category/".$image_name;
+                                                     $destination_path="../images/category/".$image_name;
 
-    //Finaly upload the image
-    $upload=move_uploaded_file($source_path,$destination_path);
+                                                     //Finaly upload the image
+                                                     $upload=move_uploaded_file($source_path,$destination_path);
 
-    //Check whether the image is uploaded or not
-    //And if the image is not uploaded than we will stop the processand redirect with error message
-    if($upload==false)
-    {   
-        //Set message
-        $_SESSION['upload']="<div class='error'>Failed to Upload Image</div>";
+                                                         //Check whether the image is uploaded or not
+                                                         //And if the image is not uploaded than we will stop the processand redirect with error message
+                                                         if($upload==false)
+                                                             {   
+                                                             //Set message
+                                                             $_SESSION['upload']="<div class='error'>Failed to Upload Image</div>";
 
-         //Redirect to category page
-         header('location:'.SITEURL.'admin/add-category.php');
-        //Stop the process
-        die();
-    }
-
-    }
-    else{
-        //Dont Upload image and set the image_name value is blank
-        $image_name="";
-    }
+                                                             //Redirect to category page
+                                                             header('location:'.SITEURL.'admin/add-category.php');
+                                                             //Stop the process
+                                                              die();
+                                                             }
+                                                 }
+                             }
+                                                             else{
+                                                              //Dont Upload image and set the image_name value is blank
+                                                              $image_name="";
+                                                                  }
 
     // 2. Create SQL Query to Insert the Category into Database
     $sql="INSERT INTO tbl_category SET
@@ -140,21 +143,21 @@ if(isset($_POST['submit']))
     // 3.Execute the query and save in Database
     $res=mysqli_query($conn, $sql);
 
-    // 4.Check whether the query executed or not and data added or not
-    if($res==true)
-    {
-        // Query Executed and Category Added
-        $_SESSION['add']="<div class='success'>Category Added Successfully</div>";
+          // 4.Check whether the query executed or not and data added or not
+          if($res==true)
+             {
+                // Query Executed and Category Added
+                 $_SESSION['add']="<div class='success'>Category Added Successfully</div>";
 
-        // Redirect to Manage Category Page
-        header('location:'.SITEURL.'admin/manage-category.php');
-    }
-    else{
-        // Failed to Add Category
-        $_SESSION['add']="<div class='error'>Failed to Add Category</div>";
-         // Redirect to Manage Category Page
-         header('location:'.SITEURL.'admin/add-category.php');
-    }
+                // Redirect to Manage Category Page
+                 header('location:'.SITEURL.'admin/manage-category.php');
+             }
+         else{
+                 // Failed to Add Category
+                 $_SESSION['add']="<div class='error'>Failed to Add Category</div>";
+                 // Redirect to Manage Category Page
+                 header('location:'.SITEURL.'admin/add-category.php');
+             }
 }
 
 
